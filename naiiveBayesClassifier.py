9a5e3ac922d336f54469f3f3d88368be11c28f69
MohamedA-M-Hassan <anehmr0@gmail.com>
2017-04-13 00:17:42 +0200

    calculate prior

--- naiiveBayesClassifier.py
+++ naiiveBayesClassifier.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def readFromFile(fileName):
 	array = []
 	with open(fileName, "r") as ins:
@@ -5,7 +7,23 @@ def readFromFile(fileName):
 			array.append(line)
 	return array
 
+def prior_calculation(string_stream):
+	no_of_spam=0
+	no_of_ham=0 # not spam
 
-list =readFromFile("train_data") 
-for row in list:
-	print("row : ",row)
+	for row in string_stream:
+		for word in row.split():
+			if word == "spam":
+				no_of_spam = no_of_spam+1
+			elif word == "ham":
+				no_of_ham = no_of_ham+1
+	total = no_of_ham + no_of_spam
+	return no_of_spam/total , no_of_ham/total
+
+
+
+
+
+myList =readFromFile("train_data") 
+spam , ham,= prior_calculation(myList)
+print ("spam: " ,spam, " ham: ", ham)	
